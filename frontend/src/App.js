04f05b0386e8154aa91989f2c3dac89f000f07d6
kerbo5000 <kerbysd@hotmail.com>
2022-06-28 13:31:31 -0400

    frontend register

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Register from './Register'
+import Container from 'react-bootstrap/Container';
+import './App.css'
+
+function App() {
+  return (
+    <Container>
+      <div class="jumbotron">
+        <Register/>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
