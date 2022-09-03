b1890a367560d83762aa8a5df3ae665d6cd2a8e7
kerbo5000 <kerbysd@hotmail.com>
2022-09-02 21:45:14 -0400

    add heroku link allowedOrigin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "https://www.fakewebsite.com",
   "http://localhost:3500",
   "http://localhost:3000",
+  "https://mernprojectkerby.herokuapp.comgit "
 ];
 module.exports = allowedOrigins;
