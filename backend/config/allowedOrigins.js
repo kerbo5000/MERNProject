43787bb86e5df93313423ecda41acdaf53202ea5
kerbo5000 <kerbysd@hotmail.com>
2022-09-02 22:17:06 -0400

    hoping for the best

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,6 @@ const allowedOrigins = [
   "https://www.fakewebsite.com",
   "http://localhost:3500",
   "http://localhost:3000",
-  "https://mernprojectkerby.herokuapp.comgit "
+  "https://mernprojectkerby.herokuapp.com"
 ];
 module.exports = allowedOrigins;
